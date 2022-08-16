8232912b5644fa8641f4a1dfbd5ad6e60a019916
Lucas Azevedo <lhs_azevedo@hotmail.com>
2022-08-16 16:59:48 -0300

    Handle sum results greater than 10

---
+++ 04-soma2/main.asm
@@ -0,0 +1,78 @@
+.MEMORYMAP
+DEFAULTSLOT 0
+SLOT 0 $4000 $0400
+.ENDME
+
+.ROMBANKSIZE $400
+.ROMBANKS 1
+
+.INCLUDE "../include/msx.asm"
+
+; Variáveis
+.ENUM $c000
+    num1: db
+.ENDE
+
+.db "AB"     ; ID for auto-executable ROM
+.dw main     ; Main program execution address.
+.dw 0        ; STATEMENT
+.dw 0        ; DEVICE
+.dw 0        ; TEXT
+.dw 0,0,0    ; Reserved
+
+pergunta1:
+.db "Digite um numero: \0"
+
+pergunta2:
+.db "Digite outro: \0"
+
+main:
+    ; Inicializa o video
+    ld a, 32
+    ld (LINL32), a ; 32 columns
+    call INIT32     ; SCREEN 1
+
+    ld hl, pergunta1
+    call puts
+
+    call INLIN
+    inc hl
+    ld a, (hl)
+    sub '0'
+    ld (num1), a
+
+    ld hl, pergunta2
+    call puts
+
+    call INLIN
+    inc hl
+    ld a, (hl)
+    sub '0'
+    ld hl, num1
+    add a, (hl)
+
+    daa
+    push af
+    rrca
+    rrca
+    rrca
+    rrca
+    and 0x0f
+    add a, '0'
+    call CHPUT
+
+    pop af
+    and 0x0f
+    add a, '0'
+    call CHPUT
+
+    di
+    halt
+
+puts:
+    ld a, (hl)
+    or a
+    ret z
+    call CHPUT
+    inc hl
+    jr puts
