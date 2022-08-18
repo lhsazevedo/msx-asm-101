de0673ec2a4292e60653eef614d28d75347beb45
Lucas Azevedo <lhs_azevedo@hotmail.com>
2022-08-18 18:29:58 -0300

    Finishes BCD sum

--- 05-soma/main.asm
+++ 05-soma/main.asm
@@ -8,33 +8,36 @@ SLOT 0 $4000 $0400
 
 .INCLUDE "../include/msx.asm"
 
+.DEFINE BCD_SIZE 4
+
 ; Variáveis
 .ENUM $c000
-    num1: dw
-    num2: dw
+    num1: dsb BCD_SIZE
+    num2: dsb BCD_SIZE
 .ENDE
 
 .db "AB"     ; ID for auto-executable ROM
-.dw main     ; Main program execution address.
+.dw init     ; Main program execution address.
 .dw 0        ; STATEMENT
 .dw 0        ; DEVICE
 .dw 0        ; TEXT
 .dw 0,0,0    ; Reserved
 
-main:
+init:
     ; Inicializa o video
     ld a, 32
     ld (LINL32), a ; 32 columns
-    call INIT32     ; SCREEN 1
+    call INIT32    ; SCREEN 1
 
+main:
     ld hl, pergunta1
     call puts
 
     call INLIN
     inc hl
     ex de, hl
-    call atoi
-    ld (num1), hl
+    ld hl, num1
+    call atobcd
 
     ld hl, pergunta2
     call puts
@@ -42,85 +45,21 @@ main:
     call INLIN
     inc hl
     ex de, hl
-    call atoi
-    
-    ld bc, (num1)
+    ld hl, num2
+    call atobcd
 
-    add hl, bc
+    ld hl, num1
+    ld de, num2
 
-    ld (num2), hl
+    call sumbcd
 
-    ld a, l
-    daa
-    ld l, a
+    ld hl, result
+    call puts
 
-    ld a, h
-    adc a, 0
-    ld h, a
+    ld hl, num1
+    call printbcd
 
-    ld a, h
-    rrca
-    rrca
-    rrca
-    rrca
-    and 0x0f
-    add a, '0'
-    push hl
-    call CHPUT
-    pop hl
-
-    ld a, h
-    and 0x0f
-    add a, '0'
-    call CHPUT
-
-    ld a, l
-    rrca
-    rrca
-    rrca
-    rrca
-    and 0x0f
-    add a, '0'
-    push hl
-    call CHPUT
-    pop hl
-
-    ld a, l
-    and 0x0f
-    add a, '0'
-    call CHPUT
-
-    ; ld a, (hl)
-    ; sub '0'
-    ; ld (num1), a
-
-    ; ld hl, pergunta2
-    ; call puts
-
-    ; call INLIN
-    ; inc hl
-    ; ld a, (hl)
-    ; sub '0'
-    ; ld hl, num1
-    ; add a, (hl)
-
-    ; daa
-    ; push af
-    ; rrca
-    ; rrca
-    ; rrca
-    ; rrca
-    ; and 0x0f
-    ; add a, '0'
-    ; call CHPUT
-
-    ; pop af
-    ; and 0x0f
-    ; add a, '0'
-    ; call CHPUT
-
-    di
-    halt
+    jr main
 
 puts:
     ld a, (hl)
@@ -130,57 +69,153 @@ puts:
     inc hl
     jr puts
 
-; DE: input string
-; HL: result
-atoi:
-    ld hl, 0
+; @param de pointer to input string
+; @paran hl pointer to result
+atobcd:
+    push hl
+    ld b, 0
 
     -:
         ld a, (de)
         or a
-        ret z
-
-        ld b, h
-        ld c, l
-        push de
-        ld de, $0A
-        call multiply
-        pop de
-
-        sub '0'
-        ld b, 0
-        ld c, a
-        add hl, bc
+        jr z, +
         inc de
+        inc b
     jr -
 
-; Multiply DE and BC, result in HL
-multiply:
-    push af
++:
+    ; ld a, b
+    ; srl a
+    ; adc a, 0
+    ; ld b, a
 
-    ld a, 16
-    ld hl, 0
+    push bc
 
+    dec de
     -:
-        srl b
-        rr c
-        jr nc, +
-            add hl, de
+        ; Low nibble
+        ld a, (de)
+        sub '0'
+        ld c, a
+        dec de
+        dec b
+
+        ; High nibble
+        jr z, +
+            ld a, (de)
+            sub '0'
+            sla a
+            sla a
+            sla a
+            sla a
+            or c
+            dec b
         +:
 
-        ex de, hl
-        add hl, hl
-        ex de, hl
-        
-        dec a
-    jr nz,-
+        ld (hl), a
+        inc hl
+        dec de
+        ld a, b
+        or a
+    jr nz, -
 
+    ; Maybe read and clear destination at the same time?
+
+    ; Pushed from BC
     pop af
+
+    bit 0, a
+    jr z, +
+        add $01
+    +:
+    srl a
+    sub BCD_SIZE
+    -:
+        ld (hl), $00
+        inc hl
+        inc a
+    jr nz, -
+
+    pop hl
+
     ret
 
+; Sum hl with de and store the result in hl
+;
+; @param hl Pointer to first BCD and result
+; @param de Pointer to second BCD
+sumbcd:
+    ; Clear carry flag
+    or a
+
+    ld b, BCD_SIZE
+    -:
+        ld a, (de)
+        adc a, (hl)
+        daa
+        ld (hl), a
+        inc hl
+        inc de
+    djnz -
+    
+
+    ret
+
+; @param hl Pointer to BCD
+printbcd:
+    ld b, 0
+    ld c, 3
+    add hl, bc
+
+    ld b, BCD_SIZE
+    sla b
+
+    ; Skip leading zeroes
+    -:
+        ld a, (hl)
+        and $f0
+        jr nz, +
+
+        dec b
+        ld a, (hl)
+        or a
+        jr nz, +
+
+        ld a, b
+        cp $01
+        jr z, +
+
+        dec hl
+    djnz -
+    +:
+
+    ; Print digits
+    -:
+        ld a, (hl)
+
+        bit 0, b
+        jr nz, +
+            srl a
+            srl a
+            srl a
+            srl a
+            jr ++
+        +:
+            and 0x0f
+            dec hl
+        ++:
+
+        add a, '0'
+        call CHPUT
+    djnz -
+
+    ret
 
 pergunta1:
 .db "Digite um numero: \0"
 
 pergunta2:
 .db "Digite outro: \0"
+
+result:
+.db "Resultado: \0"
